042c0faa20cab3b1cf05bce750684c0189087e15
Ubuntu <ubuntu@ip-10-42-10-22.ec2.internal>
2026-09-10 10:09:21 +0000

    Add workshop application v1.0

---
+++ app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "======================================"
+echo "     GITOPS WORKSHOP APPLICATION"
+echo "======================================"
+echo ""
+echo "Version: 1.0"
+echo "Participant: Student 8"
+echo ""
+echo "Application test passed."
